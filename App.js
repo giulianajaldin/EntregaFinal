4be857e7420909f5f12eb6c305076519e7896aa9
giulianajaldin <giulianajaldin.03@gmail.com>
2023-12-13 23:31:02 -0300

    actividadClase7

--- App.js
+++ App.js
@@ -1,101 +1,40 @@
-import { useState } from "react"
-import { View , Text, StyleSheet,TextInput, Button,FlatList,Modal } from "react-native"
-import uuid from 'react-native-uuid'
-//import ModalDelete from "./src/components/ModalDelete"
+import { StyleSheet, View } from 'react-native'
+import Home from './src/Screens/Home'
+import ItemListCategories from './src/Screens/ItemListCategories'
+import ItemDetail from './src/Screens/ItemDetail'
+import { useState } from 'react'
+import { useFonts } from "expo-font"
 
-export default function App() {
+const  App = () => {
+  const [categorySelected,setCategorySelected] = useState("")
   
-  const [newStudentName,setNewStudentName] = useState("")
-  const [newMarkPercent,setNewMarkPercent] = useState("")
-  const [marks,setMarks] = useState([])
-  const [productSelected,setProductSelected] = useState({})
-  const [modalVisible,setModalVisible] = useState(false) 
+  const [fontLoaded] = useFonts({
+    Josefin:require("actclase7/assets/Fonts/JosefinSans-Bold.ttf")
+  })
 
-  const handlerAddProduct = () => {
-      const newProduct = {
-        id:uuid.v4(),
-        title:newStudentName,
-        price:newMarkPercent,
+  if(!fontLoaded) return null
+
+  
+  return (
+    <View style={styles.container}>
+      {categorySelected ?
+        <ItemListCategories category = {categorySelected}/>
+        :
+        <Home setCategorySelected={setCategorySelected}/>
       }
-      setMarks(current => [...current,newProduct] )
-      setNewStudentName("")
-      setNewMarkPercent("")
-  }
+     
+    </View>
+  )
+}
 
-  const handlerModal = (item) => {
-      setProductSelected(item)
-      setModalVisible(true)
-  }
-  const handlerDeleteProduct = () => {
-    setMarks(current => current.filter(product => product.id !== productSelected.id))
-    setModalVisible(false)
-  }
-    return (
-      <View  style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-        style={styles.input}
-        placeholder="Estudiante"
-        value={newStudentName}
-        onChangeText={(t)=> setNewStudentName(t)}
-        />
-        <TextInput 
-        style={styles.input}
-        placeholder="Nota %"
-        value={newMarkPercent}
-        onChangeText={(t)=> setNewMarkPercent(t)}
-        />
-        <Button title="ADD" onPress={handlerAddProduct} />
-      </View>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={marks}
-          keyExtractor={item => item.id}
-          renderItem={({item})=> <View style={styles.cardProduct}>
-                                    <Text style={styles.cardTitle}>{item.title}</Text>
-                                    <Text>{item.price} %</Text>
-                                    <Button title="DEL" onPress={() => handlerModal(item)}/>
-                                    </View> }
-        />
-      </View>
-      
-</View>
-
-    )}
+export default App
 
 
-    const styles = StyleSheet.create({
-      container:{
-        flex:1,
-        backgroundColor: 'pink',
-        justifyContent:"start",
-        alignItems:"center",
-        marginTop:25,
-      },
-      inputContainer:{
-        flexDirection:"row",
-        alignItems:"center",
-        width:"100%",
-        justifyContent:"space-around"
-      },
-      input:{
-        borderWidth:4,
-        paddingHorizontal:10,
-        paddingVertical:5,
-        width:150
-      },
-      listContainer : {
-  
-        width:"100%"
-      },
-      cardProduct:{
-  
-        flexDirection:"row",
-        padding:10,
-        margin:10,
-        justifyContent:"space-around",
-        alignItems:"center",
-        borderWidth:4
-      },
-      
-    });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'start',
+  },
+})
