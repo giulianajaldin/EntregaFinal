7ef9c417afe2b97c4c060a32200539872ffa09b8
giulianajaldin <giulianajaldin.03@gmail.com>
2024-02-13 22:46:36 -0300

    EntregaFinal

--- src/navigation/CartStack.js
+++ src/navigation/CartStack.js
@@ -5,20 +5,24 @@ import Cart from '../Screens/Cart'
 const Stack = createNativeStackNavigator()
 
 const CartStack = () => {
-  return (
-    <Stack.Navigator
-        initialRouteName='Cart'
-        screenOptions={
-            ({route})=>{
-                return {
-                    header : () => <Header title="Carrito"/>
-                }
-            }
-        }
-    >
-        <Stack.Screen name="Cart" component={Cart} />
-    </Stack.Navigator>
-  )
-}
-
+    return (
+      
+         <Stack.Navigator
+          initialRouteName='Cart'
+          screenOptions={
+              ({route})=>{
+                  return {
+                      header : () => <Header title={'Cart'}               />
+                  }
+              }
+          }
+      >
+          <Stack.Screen name="Cart" component={Cart} />
+          
+          
+        </Stack.Navigator>
+     
+    )
+  }
+//
 export default CartStack
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,70 +1,70 @@
-import { StyleSheet } from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ShopStack from './ShopStack'
-import CartStack from "./CartStack"
-import { colors } from '../Global/colors'
-import OrdersStack from './OrdersStack'
-import TabIcon from '../Components/TabIcon'
-import ProfileStack from './ProfileStack'
+import CartStack from './CartStack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from 'react-native';
+import { colors } from '../Global/colors';
+import OrdersStack from './OrdersStack';
+import ShopIcon from '../components/ShopIcon';
+import CartIcon from '../components/CartIcon';
+import OrderIcon from '../components/OrdersIcon';
+import ProfileStack from './ProfileStack';
+import ProfileIcon from '../components/ProfileIcon';
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 
 const TabNavigator = () => {
-  return (
-        <Tab.Navigator
-          screenOptions={{
-            headerShown:false,
-            tabBarShowLabel:false,
-            tabBarStyle: styles.tabBar
+    return (
 
-          }}
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: styles.tabBar
+            }}
         >
-          <Tab.Screen
-             name="ShopStack"
-             component={ShopStack}
-             options={{
-              tabBarIcon:({focused}) => <TabIcon icon="shop" label="Productos" focused={focused}/>
-             }}
-          />
-          <Tab.Screen 
-              name="CartStack" 
-              component={CartStack}
-              options={{
-                tabBarIcon:({focused}) =>  <TabIcon icon="shopping-cart" label="Carrito" focused={focused}/> 
-              }}
-             />
-             <Tab.Screen 
-              name="OrdersStack" 
-              component={OrdersStack}
-              options={{
-                tabBarIcon:({focused}) => <TabIcon icon="list" label="Ordenes" focused={focused}/> 
-              }}
-             />
-            <Tab.Screen 
-              name="ProfileStack" 
-              component={ProfileStack}
-              options={{
-                tabBarIcon:({focused}) => <TabIcon icon="user" label="Perfil" focused={focused}/> 
-              }}
-             />
-      </Tab.Navigator>
-  )
+            <Tab.Screen
+                name="ShopStack"
+                component={ShopStack}
+                options={{
+                    tabBarIcon: () => <ShopIcon />
+                }}
+            />
+
+
+            <Tab.Screen name="CartStack" component={CartStack}
+                options={{
+                    tabBarIcon: () => <CartIcon />
+                }}
+            />
+            <Tab.Screen name="OrdersStack" component={OrdersStack}
+                options={{
+                    tabBarIcon: () => <OrderIcon />
+                }}
+            />
+            <Tab.Screen name="ProfileStack" component={ProfileStack}
+                options={{
+                    tabBarIcon: () => <ProfileIcon />
+                }}
+            />
+        </Tab.Navigator>
+
+    )
 }
+
 
 export default TabNavigator
 
 const styles = StyleSheet.create({
-    tabBar:{
-      backgroundColor:colors.green3,
-      elevation:4,
-      position:"absolute",
-      bottom:25,
-      left:20,
-      right:20,
-      borderRadius:15,
-      height:90
-
-
+    tabBar: {
+        backgroundColor: colors.lavender,
+        shadowColor: 'black',
+        paddingBottom:20,
+        position: 'absolute',
+        height: 110,
+        opacity: 0.8,
+        borderTopWidth:3,
+        borderTopColor:"black"
     }
 })
+//
